01c32caa67b44490b396e3133eba31eed08f9583
Ryan Lim <ljxr1337@gmail.com>
2020-08-13 13:52:29 +0800

    added stuff

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def root():
-    return render_template('index.html', name='ryan')
+    return render_template('index.html')
 
 @app.route('/newgame')
 def newgame():
-    return "Starting a new game"
+    return render_template('chess.html', player="white player")
 app.run()
